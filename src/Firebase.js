4ec7362bfd65356e55dc97003bd99e52374f8f38
DinethP <dineth_perera@hotmail.com>
2020-09-05 23:18:50 +0530

    Built Sidebar component

---
+++ src/Firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAAN7tg90KjhZIR90kukZoHodD8kGcnugk",
+  authDomain: "whatsapp-mern-e5f2a.firebaseapp.com",
+  databaseURL: "https://whatsapp-mern-e5f2a.firebaseio.com",
+  projectId: "whatsapp-mern-e5f2a",
+  storageBucket: "whatsapp-mern-e5f2a.appspot.com",
+  messagingSenderId: "227450814912",
+  appId: "1:227450814912:web:bdcd071582981f3927c825",
+  measurementId: "G-JYEN957F09"
+};
---
+++ src/components/Chat.js
@@ -0,0 +1,35 @@
+import React from "react";
+import "./Chat.css";
+import { Avatar, IconButton } from "@material-ui/core";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+import AttachFile from "@material-ui/icons/AttachFile";
+import SearchOutlined from "@material-ui/icons/SearchOutlined";
+
+function Chat() {
+  return (
+    <div className="chat">
+      <div className="chat__header">
+        <Avatar />
+
+        <div className="chat__headerInfo">
+          <h3>Room name</h3>
+          <p>Last seem at...</p>
+        </div>
+
+        <div className="chat__headerRight">
+        <IconButton>
+            <SearchOutlined />
+          </IconButton>
+          <IconButton>
+            <AttachFile />
+          </IconButton>
+          <IconButton>
+            <MoreVertIcon />
+          </IconButton>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Chat;
