59c143a0625d280abb6060a854dce645a78d2280
DinethP <dineth_perera@hotmail.com>
2020-09-06 19:17:39 +0530

    styling chat-footer

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -4,6 +4,9 @@ import { Avatar, IconButton } from "@material-ui/core";
 import MoreVertIcon from "@material-ui/icons/MoreVert";
 import AttachFile from "@material-ui/icons/AttachFile";
 import SearchOutlined from "@material-ui/icons/SearchOutlined";
+import InsertEmoticonIcon from "@material-ui/icons/InsertEmoticon";
+import MicIcon from "@material-ui/icons/Mic";
+
 
 function Chat() {
   return (
@@ -37,7 +40,22 @@ function Chat() {
             {new Date().toUTCString()}
           </span>
         </p>
+        <p className="chat__message chat__receiver">
+          <span className="chat__name">Dineth</span>
+          This is a message
+          <span className="chat__timestamp">
+            {new Date().toUTCString()}
+          </span>
+        </p>
         
+      </div>
+      <div className="chat__footer">
+        <InsertEmoticonIcon />
+        <form action="">
+          <input type="text" placeholder="Type a message"/>
+          <button type="submit" >Send a message</button>
+        </form>
+        <MicIcon />
       </div>
     </div>
   );
