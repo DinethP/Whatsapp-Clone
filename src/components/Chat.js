ff1116979a49bac3a0de8b29e7ac9af32bde2ec8
DinethP <dineth_perera@hotmail.com>
2020-09-06 08:51:48 +0530

    working on chat-body styling

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -17,7 +17,7 @@ function Chat() {
         </div>
 
         <div className="chat__headerRight">
-        <IconButton>
+          <IconButton>
             <SearchOutlined />
           </IconButton>
           <IconButton>
@@ -27,6 +27,17 @@ function Chat() {
             <MoreVertIcon />
           </IconButton>
         </div>
+      </div>
+
+      <div className="chat__body">
+        <p className="chat__message">
+          <span className="chat__name">Dineth</span>
+          This is a message
+          <span className="chat__timestamp">
+            {new Date().toUTCString()}
+          </span>
+        </p>
+        
       </div>
     </div>
   );
